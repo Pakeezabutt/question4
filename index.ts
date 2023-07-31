93ac3d165fae72b821cd17696ed4b24f62f39a59
Pakeezabutt <139985837+Pakeezabutt@users.noreply.github.com>
2023-07-31 06:07:46 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,7 @@
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like the following, including the quotation marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+// Heraclitus once said, “No man ever steps in the same river twice, for it's not the same river and he's not the same man.”
+
+console.log('Heraclitus once said, “No man ever steps in the same river twice, for its not the same river and he is not the same man.”')
